5e382ce76eb0dee372c81a6791e7e607fc23a217
John-Dag Addy <jaedagy@gmail.com>
2023-07-27 21:39:21 +0000

    fix: add function to  repackDecimals in  forms

--- src/misc/utils.ts
+++ src/misc/utils.ts
@@ -11,3 +11,11 @@ export const parsePhoneNumber = (phoneNumber: string) => {
     .replace(')', '')
     .replace('-', '')
 }
+
+export const repackDecimals = (decimal: string | number) => {
+  if (decimal === 0 || decimal === '0.0' || !decimal) {
+    return '0.0'
+  }
+
+  return parseFloat(decimal.toString())
+}
