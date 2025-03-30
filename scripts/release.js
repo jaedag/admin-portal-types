ea08c2737adf1eb8d212bd978f003cb35dbf360c
John-Dag Addy <jaedagy@gmail.com>
2025-03-30 16:45:46 +0000

    chore: update release script to publish with public access

--- scripts/release.js
+++ scripts/release.js
@@ -6,7 +6,7 @@ const changelog = []
 const publish = [
   {
     name: 'publish',
-    command: 'npm run rollup && npm publish',
+    command: 'npm run rollup && npm publish  --access public',
     prefixColor: 'red',
   },
 ]
